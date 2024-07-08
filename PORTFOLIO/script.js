12068c5e480542b3a02c2d1b8d433a20befd953d
Sridharan-AR4 <sridhar47914@gmail.com>
2024-07-08 21:13:00 +0530

    added projects

--- PORTFOLIO/script.js
+++ PORTFOLIO/script.js
@@ -1,5 +1,14 @@
+
+window.onload = function() {window.scrollTo(0,0);}
 var menulist=document.getElementById("navlist");
 var menubar=document.querySelector(".menubar");
 menubar.addEventListener("click",()=>{
     menulist.classList.toggle("showhide");
+})
+menulist.addEventListener("click",()=>{
+    menulist.classList.toggle("showhide");
+})
+var contact_form=document.getElementById("contact_form");
+contact_form.addEventListener("submit",(e)=>{
+    e.preventDefault();
 })
