4b6e6fcda6599e99eef62d1f37527fd7681f3bbe
Sridharan-AR4 <sridhar47914@gmail.com>
2024-07-11 16:16:34 +0530

    comleted

--- PORTFOLIO/script.js
+++ PORTFOLIO/script.js
@@ -9,6 +9,8 @@ menulist.addEventListener("click",()=>{
     menulist.classList.toggle("showhide");
 })
 var contact_form=document.getElementById("contact_form");
+var contact_ovrlay=document.querySelector(".contact_ovrlay");
 contact_form.addEventListener("submit",(e)=>{
     e.preventDefault();
+    window.location.href="contact.html";
 })
